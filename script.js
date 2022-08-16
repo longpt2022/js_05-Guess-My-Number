8df5a92560b815efe371f297683a25c6c9071679
longpt271 <longpt271@gmail.com>
2022-08-16 21:05:32 +0700

    Xử lý Click Events

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-'use strict';
+"use strict";
+
+//------------- Xử lý Click Events
+// - Xử lý dự kiện click nút '.check'. Nhận giá trị guess khi nhấn
+// - Nếu chưa nhập số in ra lỗi "if (!guess) {}"
+
+// trong addEventListener() function() xử lý khi có sự kiện click
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+
+  // Nếu chưa nhập số nào mà click
+  if (!guess) {
+    document.querySelector(".message").textContent = "⛔ No number!";
+  }
+});
